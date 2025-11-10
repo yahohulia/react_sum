081cab4c6e7f99726f27289aec9cffef7a416eb1
yahohulia <yaroslavhohulia@gmail.com>
2025-11-10 16:21:58 +0200

    changed <p> in Sum

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -2,8 +2,6 @@ export const Sum = ({ a = 0, b = 0 }) => {
   const sum = a + b;
 
   return (
-    <p>
-      Sum of {a} and {b} is {sum}
-    </p>
+    <p>{`Sum of ${a} and ${b} is ${sum}`}</p>
   );
 };
